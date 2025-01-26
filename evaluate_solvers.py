d897ccb08b0fd782e4205aac149149e9be53fbca
Orin Pour <66479747+Orin111@users.noreply.github.com>
2025-01-26 18:22:16 +0200

    Add files via upload

---
+++ evaluate_solvers.py
@@ -0,0 +1,223 @@
+from flow_game.flow_free_problem import FlowFreeProblem
+from main import run_search_algorithm
+from main import convert_dots_to_sat_problem
+from solvers.SAT import FlowFreeSAT
+from problems.level_creator import create_level
+import signal
+import pandas as pd
+import time
+
+SAT_FAILED = "UNSAT"
+LINE_FORMAT_ERROR = "Unexpected line format"
+NUMBER_OF_LEVELS_PER_GRID_SIZE = 10
+TIMEOUT = 180
+
+sat_results = pd.DataFrame(columns=['grid size','level', 'time'])
+search_results = pd.DataFrame(columns=['grid size', 'level', 'time', 'expended nodes', 'algorithm'])
+
+class TimeoutException(Exception):
+    """
+    Class for a timeout exception.
+    """
+    pass
+
+def timeout_handler(signum, frame):
+    """
+    Handle a timeout signal.
+    :param signum: signal number
+    :param frame: frame
+    :return: None
+    """
+    raise TimeoutException()
+
+
+# Set up the timeout handler
+signal.signal(signal.SIGALRM, timeout_handler)
+
+
+def run_with_timeout(timeout, function, *args, **kwargs):
+    """
+    Run a function with a timeout.
+    :param timeout: timeout in seconds
+    :param function: function to run
+    :param args: arguments
+    :param kwargs: keyword arguments
+    :return: the result of the function or None if it times out
+    """
+    signal.alarm(timeout)  # Set the timeout in seconds
+    try:
+        result = function(*args, **kwargs)
+    except TimeoutException:
+        result = None
+    finally:
+        signal.alarm(0)  # Disable the alarm
+    return result
+
+
+def load_levels(grid_size):
+    """
+    Load the levels for the given grid size.
+    :param grid_size: size of the grid
+    :return: list of levels
+    """
+    # iterate over the files in problems directory,
+    # use process_image to convert the image of the specific grid size to a
+    # list of lists where each sublist represents a level
+    loaded_levels = []
+    for i in range(NUMBER_OF_LEVELS_PER_GRID_SIZE):
+        level = create_level(grid_size, i+1)
+        loaded_levels.append(level)
+
+    return loaded_levels
+
+
+def print_results(algorithm, problems_not_passed_lst,
+                  num_of_passed_problems, total_levels):
+    """
+    Print the results of the algorithm.
+    :param algorithm: name of the algorithm
+    :param problems_not_passed_lst: list of problems not passed
+    :param num_of_passed_problems: number of problems passed
+    :param total_levels: number of levels that were tested
+    :return:
+    """
+    print(f'{algorithm} passed {num_of_passed_problems}/{total_levels}')
+    if problems_not_passed_lst:
+        print(f'problems not passed: {problems_not_passed_lst}')
+    print('---------------------------------------------------')
+
+
+
+def evaluate_sat_solver(levels_list, grid_size):
+    """
+    Evaluate the SAT solver on the given levels.
+    :param levels_list: list of levels
+    :param grid_size: size of the grid
+    """
+    global sat_results
+    num_of_passed_problems = 0
+    problems_not_passed_lst = []
+
+    for level_num, level in enumerate(levels_list):
+        colors, initial_board = convert_dots_to_sat_problem(level)
+        sat_solver = FlowFreeSAT(grid_size, colors, initial_board)
+        # start timer
+        start_time = time.time()
+        solution = sat_solver.solve(level)
+        elapsed_time = time.time() - start_time
+        sat_results.loc[len(sat_results)] = [grid_size, level_num + 1, elapsed_time]
+        if solution == SAT_FAILED:
+            problems_not_passed_lst.append(level_num + 1)
+            continue
+        num_of_passed_problems += 1
+    print_results("SAT", problems_not_passed_lst, num_of_passed_problems, len(levels_list))
+
+
+def solve_search_problem(algo, problem_dots, grid_size):
+    """
+    Solve the search problem using the given algorithm.
+    :param algo: algorithm to use
+    :param problem_dots: list of dots representing the problem
+    :param grid_size: size of the grid
+    :return: the goal state of the problem
+    """
+    problem = FlowFreeProblem(grid_size, problem_dots)
+    actions = run_with_timeout(TIMEOUT, run_search_algorithm, algo, problem)
+    problem_state = problem.get_start_state()
+    if actions is not None:
+        for action in actions:
+            problem_state = problem_state.do_move(action)
+    return problem_state, problem.expanded
+
+
+def evaluate_search_algorithm(levels_list, grid_size):
+    """
+    Evaluate the search algorithm on the given levels.
+    :param levels_list: list of levels
+    :param grid_size: size of the grid
+    """
+    search_algorithms = ["BFS", "DFS", "UCS", "A*"]
+    passed_problems = {algo: 0 for algo in search_algorithms}
+    global search_results
+    for algo in search_algorithms:
+        problems_not_passed_lst = []
+        problems_passed_counter = 0
+        for level_num, level in enumerate(levels_list):
+            start_time = time.time()
+            solution, expended = solve_search_problem(algo, level, grid_size)
+            elapsed_time = time.time() - start_time
+            if solution.is_goal_state():
+                problems_passed_counter += 1
+            else:
+                problems_not_passed_lst.append(level_num + 1)
+            search_results.loc[len(search_results)] = [grid_size, level_num + 1, elapsed_time, expended, algo]
+        passed_problems[algo] = problems_passed_counter
+        print_results(algo, problems_not_passed_lst,
+                      problems_passed_counter, len(levels_list))
+
+solvers = {"Search": evaluate_search_algorithm,
+           "SAT": evaluate_sat_solver}
+
+
+def evaluate_all(grid_size, levels_list):
+    """
+    Evaluate all algorithms on the given levels.
+    :param grid_size: size of the grid
+    :param levels_list: list of levels
+    """
+    for solver_type in solvers.keys():
+            solvers.get(solver_type)(levels_list, grid_size)
+
+def evaluate_algorithms_for_grid_size(algorithm_name, grid_size, levels_list):
+    """
+    Evaluate the algorithms for the given grid size.
+    :param algorithm_name: type of algorithm to evaluate
+    :param grid_size: size of the grid
+    :param levels_list: list of levels
+    """
+    if algorithm_name == "All":
+        evaluate_all(grid_size, levels_list)
+    else:
+        solvers.get(algorithm_name)(levels_list, grid_size)
+
+def save_dataframe(algorithm_name):
+    """
+    Save the results to a csv file.
+    :param algorithm_name: name of the algorithm
+    """
+    convert_pd_types()
+    dataframes = []
+    # save the dataframe to a csv file
+    if algorithm_name == "SAT" or algorithm_name == "All":
+        dataframes.append((sat_results, 'SAT'))
+    if algorithm_name == "Search" or algorithm_name == "All":
+        dataframes.append((search_results, 'Search'))
+    for dataframe, algorithm_name in dataframes:
+        dataframe.to_csv(f'results//{algorithm_name}_results.csv', index=False)
+
+
+def convert_pd_types():
+    """
+    Convert the types of the dataframes.
+    """
+    global sat_results, search_results
+    sat_results = sat_results.astype(
+        {'grid size': 'int64', 'level': 'int64', 'time': 'float64'})
+    search_results = search_results.astype({'grid size': 'int64', 'level': 'int64',
+                                            'time': 'float32', 'expended nodes': 'int64',
+                                            'algorithm': 'string'})
+
+
+if __name__ == "__main__":
+    algorithm_type = "Search"
+    min_grid_size = 5
+    max_grid_size = 5
+    for size in range(min_grid_size, max_grid_size + 1):
+        print("Simulating algorithms for grid size:", size)
+        levels = load_levels(size)
+        evaluate_algorithms_for_grid_size(algorithm_type, size, levels)
+        print()
+    save_dataframe(algorithm_type)
+
+
+
---
+++ main.py
@@ -0,0 +1,232 @@
+import tkinter as tk
+import time
+
+from flow_game.flow_free_problem import FlowFreeProblem
+from problems.level_creator import create_level
+from solvers import solver
+from solvers.SAT import FlowFreeSAT
+from solvers.q_learning_agent import QLearningAgent
+from solvers.flow_free_env import FlowFreeEnvironment
+from gui import FlowFreeGUI
+import solvers.heuristics as heuristics
+from solvers.approx_q_learning import ApproxQLearningAgent
+
+SAT_NO_SOLUTION = "UNSAT"
+NO_SOLUTION_MESSAGE = "No solution found."
+SOLUTION_FOUND_MESSAGE = "Solution found."
+
+
+def run_search_algorithm(search_algorithm_name, problem):
+    """
+    Run the search algorithm on the given problem.
+    :param search_algorithm_name: name of the search algorithm to run
+    :param problem: the problem to solve
+    :return: a list of actions that reaches the goal state
+    """
+    if search_algorithm_name == "DFS":
+        return solver.depth_first_search(problem)
+    elif search_algorithm_name == "UCS":
+        return solver.uniform_cost_search(problem)
+    elif search_algorithm_name == "A*":
+        return solver.a_star_search(problem, heuristics.combined_heuristic)
+    elif search_algorithm_name == "BFS":
+        return solver.breadth_first_search(problem)
+
+
+def choose_rl_agent(agent_name):
+    """
+    Choose the reinforcement learning agent to use.
+    :param agent_name: name of the agent to use
+    :return: the chosen agent
+    """
+    if agent_name == "Q learning":
+        return QLearningAgent(0.7, 0.9, 0.75)
+    elif agent_name == "AQ learning":
+        return ApproxQLearningAgent()
+
+
+def start_search(problem, algorithm_name):
+    """
+    Start the search algorithm on the given problem.
+    :param problem: the problem to solve
+    :param algorithm_name: the name of the algorithm to use
+    :return: a list of actions that reaches the goal state and the time taken
+    """
+    start_time = time.time()
+    actions = run_search_algorithm(algorithm_name, problem)
+    elapsed_time = time.time() - start_time
+    print(f"result(time,expended nodes): {elapsed_time},{problem.expanded}")
+    return actions, elapsed_time
+
+
+def execute_actions_with_delay(gui, curr_state, actions, index=0):
+    """
+    Execute the actions with a delay to display the moves on the GUI.
+    :param gui: gui object to display the moves
+    :param curr_state: current state of the game
+    :param actions: list of actions to execute
+    :param index: index of the current action
+    """
+    if index < len(actions):
+        action = actions[index]
+        curr_state = curr_state.do_move(action)
+        gui.update_board(curr_state.game_board)
+        gui.root.after(500, lambda: execute_actions_with_delay(gui, curr_state,
+                                                               actions,
+                                                               index + 1))
+    else:
+        gui.display_win_message()  # Display "You Win" message when done
+
+
+def solve_with_search(algorithm_name, problem, dots_list):
+    """
+    Solve the problem using the search algorithm and display the GUI.
+    :param algorithm_name: name of the algorithm to use
+    :param problem: the problem to solve
+    :param dots_list: list of dots representing the board initial state
+    """
+    # Execute the solvers algorithm
+    actions, elapsed_time = start_search(problem, algorithm_name)
+    display_gui(problem, algorithm_name, actions, elapsed_time)
+
+
+def solve_with_rl(algorithm_name, problem, dots_list):
+    """
+    Solve the problem using the reinforcement learning agent and display the GUI.
+    :param algorithm_name: name of the agent to use
+    :param problem: the problem to solve
+    :param dots_list: list of dots representing the board initial state
+    """
+    environment = FlowFreeEnvironment(problem)
+    # Execute the solvers algorithm
+    agent = choose_rl_agent(algorithm_name)
+    # Train the agent
+    agent.train(episodes=10000, environment=environment)
+    # Solve a specific level
+    initial_state = environment.reset()
+    start_time = time.time()
+    actions = agent.solve(initial_state, environment)
+    elapsed_time = time.time() - start_time
+    display_gui(problem, algorithm_name, actions, elapsed_time)
+
+
+def convert_dots_to_sat_problem(dots):
+    """
+    Convert the dots list to a SAT problem format.
+    :param dots: list of Dot objects representing the board initial state
+    :return: list of colors and a dictionary representing the board
+    """
+    dot_dict = {}
+    colors = set()
+    for dot in dots:
+        colors.add(dot.get_color())
+        pos = (dot.get_x(), dot.get_y())
+        dot_dict[pos] = dot.get_color()
+    return list(colors), dot_dict
+
+
+def solve_with_sat(algorithm_name, problem, dots_list):
+    """
+    Solve the problem using the SAT solver and display the GUI.
+    :param algorithm_name: name of the algorithm to use
+    :param problem: the problem to solve
+    :param dots_list: list of dots representing the board initial state
+    :return:
+    """
+    board_size, dots_array = problem.get_problem_vars()
+    colors, initial_board = convert_dots_to_sat_problem(dots_array)
+    sat_solver = FlowFreeSAT(board_size, colors, initial_board)
+
+    start_time = time.time()
+    solution = sat_solver.solve(dots_list)
+    elapsed_time = time.time() - start_time
+
+    if solution == SAT_NO_SOLUTION:
+        print(NO_SOLUTION_MESSAGE)
+    else:
+        print(SOLUTION_FOUND_MESSAGE)
+        root = tk.Tk()
+        flow_free_gui = FlowFreeGUI(root, board_size, board_size)
+        flow_free_gui.display_solved_board(sat_solver.convert_sol_to_board(
+            solution), algorithm_name, elapsed_time)
+        # Start the Tkinter event loop
+        root.mainloop()
+
+
+def display_initial_board(problem):
+    """
+    A function to display the initial board state.
+    :param problem: the problem to display
+    """
+    root = tk.Tk()
+    board_size = problem.board.board_size
+    gui = FlowFreeGUI(root, board_size, board_size)
+
+    # Display the starting dots
+    dots_list = problem.board.dots_list
+    gui.display_starting_dots(dots_list)
+
+    root.mainloop()
+
+
+def display_gui(problem, algorithm_name, actions, elapsed_time):
+    """
+    Display the GUI with the given problem and actions.
+    :param problem: the problem to display
+    :param algorithm_name: name of the algorithm used to solve the problem
+    :param actions: list of actions to execute
+    :param elapsed_time: time taken to solve the problem
+    """
+    # Create Tkinter root and GUI only after the solvers algorithm is done
+    root = tk.Tk()
+    board_size = problem.board.board_size
+    gui = FlowFreeGUI(root, board_size, board_size)
+
+    # Set the solvers time, expanded nodes, and algorithm name in the GUI
+    gui.set_search_time(elapsed_time)
+    gui.set_expanded_nodes(problem.expanded)
+    gui.set_algorithm_name(algorithm_name)
+
+    if actions:
+        # Display the initial state and start executing actions with a delay
+        curr_state = problem.get_start_state()
+        gui.update_board(curr_state.game_board)
+        root.after(500, execute_actions_with_delay, gui, curr_state, actions)
+    else:
+        gui.display_lost_message()  # Display "You Lost" message if no actions
+
+    root.mainloop()
+
+
+# dictionary of solvers
+solvers = {"A*": solve_with_search, "DFS": solve_with_search,
+           "BFS": solve_with_search, "UCS": solve_with_search,
+           "SAT": solve_with_sat, "Q learning": solve_with_rl,
+           "AQ learning": solve_with_rl}
+
+
+def solve_game(algorithm, grid_size, dots_list):
+    """
+    Solve the game using the given algorithm.
+    :param algorithm: the algorithm to use
+    :param grid_size: the size of the grid
+    :param dots_list: list of dots representing the board initial state
+    """
+    problem = FlowFreeProblem(grid_size, dots_list)
+    # display_initial_board(problem)
+    solvers.get(algorithm)(algorithm, problem, dots_list)
+
+
+def main():
+    """
+    Main function to solve a level with a specific algorithm.
+    """
+    algorithm = "SAT"
+    grid_size = 5
+    level = 5
+    dots_list = create_level(grid_size, level)
+    solve_game(algorithm, grid_size, dots_list)
+
+
+if __name__ == "__main__":
+    main()
